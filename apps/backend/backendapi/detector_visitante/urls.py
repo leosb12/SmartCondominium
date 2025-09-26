2a2f7428801b4882a2b91741b31570d790e8d1d5
Leonardo Serrate <leonardoserrate9@gmail.com>
2025-09-26 00:08:01 -0400

    no se

--- apps/backend/backendapi/detector_visitante/urls.py
+++ apps/backend/backendapi/detector_visitante/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import VisitorMatchView
 
 urlpatterns = [
-    path("visitor-data/<str:visitor_id>/", name="visitor-data"),
     path("visitors/match", VisitorMatchView.as_view(), name="visitors-match"),
 ]
